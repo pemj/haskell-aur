74ae80450055159d9477d1c92493d68cd4987aad
Colin Woodbury <colingw@gmail.com>
2014-06-04 10:13:49 -0700

    Fixed encoding problems

--- Linux/Arch/Aur.hs
+++ Linux/Arch/Aur.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Linux.Arch.Aur
        ( -- * QUERIES
          search
        , info
        , multiinfo
-       , msearch ) where
+       , msearch
+       -- * As JSON
+       , multiinfo'
+       -- * Pretty JSON
+       , pretty) where
 
 import Linux.Arch.Aur.Types
 
@@ -14,15 +19,14 @@ import Control.Lens
 import Data.Aeson           (Value(..))
 import Data.Aeson.Lens      (AsValue, _String, key, nth)
 import Data.Aeson.Encode.Pretty
---import Data.ByteString.Lazy (ByteString)
 import Data.Map.Lazy
 import Data.Maybe           (listToMaybe)
 import Data.Text
-import Data.Text.Encoding
+import Data.Text.Lazy.Encoding
 import Network.Wreq
 
---import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Vector as V
+import qualified Data.Text.Lazy as TL
 
 ---
 
@@ -43,18 +47,26 @@ info :: Text -> IO (Maybe AurInfo)
 info = undefined
 --info pkg = listToMaybe <$> multiinfo [pkg]
 
+-- | `info` call as Haskellized JSON.
+info' :: Text -> IO (Maybe Value)
+info' pkg = (>>= (^? nth 0)) <$> multiinfo' [pkg]
+
 -- | Like `info`, but can handle requests for multiple packages at once.
 -- More efficient than using `info` multiple times.
 --multiinfo :: [Text] -> IO [AurInfo]
-multiinfo pkgs = (pretty <$>) <$> fetch pkgs "multiinfo" "arg[]"
+multiinfo = multiinfo'
+
+-- | `multiinfo` call as Haskellized JSON.
+multiinfo' :: [Text] -> IO (Maybe Value)
+multiinfo' pkgs = fetch pkgs "multiinfo" "arg[]"
 
 -- | Search the AUR by Maintainer name.
 msearch :: Text -> IO [AurInfo]
 msearch = undefined
 
--- Seems we can convert the reponse to the JSON we need right away.
+-- | Call the RPC.
 fetch :: [Text] -> String -> Text -> IO (Maybe Value)
-fetch pkgs method argLabel = rpcResults <$> (getWith opts $ rpcUrl method)
+fetch pkgs method argLabel = rpcResults <$> getWith opts (rpcUrl method)
     where opts = defaults & param argLabel .~ pkgs
 
 rpcResults :: AsValue r => Response r -> Maybe Value
@@ -62,4 +74,4 @@ rpcResults r = r ^? responseBody . key "results"
 
 -- | Conversion of JSON to nicely formated text.
 pretty :: Value -> Text
-pretty = decodeUtf8 . encodePretty
+pretty = TL.toStrict . decodeUtf8 . encodePretty
