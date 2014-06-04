fa07409eea23e137b55212412ab824f99f17297f
Colin Woodbury <colingw@gmail.com>
2014-06-03 23:15:08 -0700

    Fetching JSON works
    - Still having some ByteString type issues.
      Trying to convert JSON to Text, but it seems we have to go through
      ByteString first and that is causing some problems.

--- Linux/Arch/Aur.hs
+++ Linux/Arch/Aur.hs
@@ -9,17 +9,27 @@ module Linux.Arch.Aur
 
 import Linux.Arch.Aur.Types
 
+import Control.Applicative  ((<$>))
 import Control.Lens
+import Data.Aeson           (Value(..))
+import Data.Aeson.Lens      (AsValue, _String, key, nth)
+import Data.Aeson.Encode.Pretty
+--import Data.ByteString.Lazy (ByteString)
+import Data.Map.Lazy
+import Data.Maybe           (listToMaybe)
 import Data.Text
-import Data.Aeson.Lens (_String, key)
+import Data.Text.Encoding
 import Network.Wreq
+
+--import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.Vector as V
 
 ---
 
-rpcUrl :: String
-rpcUrl = "https://aur.archlinux.org/rpc.php?"
-         ++ apiVersion
-         ++ "&type=multiinfo&"
+rpcUrl :: String -> String
+rpcUrl method = "https://aur.archlinux.org/rpc.php?"
+                ++ apiVersion
+                ++ "&type=" ++ method ++ "&"
 
 apiVersion = "v=2"
 
@@ -31,18 +41,25 @@ search = undefined
 -- | Returns all information about one package.
 info :: Text -> IO (Maybe AurInfo)
 info = undefined
+--info pkg = listToMaybe <$> multiinfo [pkg]
 
 -- | Like `info`, but can handle requests for multiple packages at once.
 -- More efficient than using `info` multiple times.
-multiinfo :: [Text] -> IO [AurInfo]
-multiinfo = undefined
+--multiinfo :: [Text] -> IO [AurInfo]
+multiinfo pkgs = (pretty <$>) <$> fetch pkgs "multiinfo" "arg[]"
 
 -- | Search the AUR by Maintainer name.
 msearch :: Text -> IO [AurInfo]
 msearch = undefined
 
---foo :: IO ()
-foo = do
-  r <- getWith opts rpcUrl
-  return $ r ^. responseBody -- . key "url" . _String
-      where opts = defaults & param "arg[]" .~ ["aura","aura-bin"]
+-- Seems we can convert the reponse to the JSON we need right away.
+fetch :: [Text] -> String -> Text -> IO (Maybe Value)
+fetch pkgs method argLabel = rpcResults <$> (getWith opts $ rpcUrl method)
+    where opts = defaults & param argLabel .~ pkgs
+
+rpcResults :: AsValue r => Response r -> Maybe Value
+rpcResults r = r ^? responseBody . key "results"
+
+-- | Conversion of JSON to nicely formated text.
+pretty :: Value -> Text
+pretty = decodeUtf8 . encodePretty
