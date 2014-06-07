01dc967a051a8a62d4646b9ef919f5fb9eb20eb8
Colin Woodbury <colingw@gmail.com>
2014-06-07 11:25:16 -0700

    Set default parse values in a more idiomatic way

--- Linux/Arch/Aur/Types.hs
+++ Linux/Arch/Aur/Types.hs
@@ -4,10 +4,7 @@ module Linux.Arch.Aur.Types
 import Control.Applicative
 import Control.Monad (mzero)
 import Data.Aeson
-import Data.Maybe (maybeToList)
 import Data.Text
-
-import qualified Data.List as L
 
 ---
 
@@ -33,26 +30,25 @@ data AurInfo = AurInfo { aurIdOf          :: Int
                        , providesOf       :: [Text] } deriving (Eq,Show)
 
 instance FromJSON AurInfo where
-    parseJSON (Object v) = AurInfo                     <$>
-                           v .: "ID"                   <*>
-                           v .: "Name"                 <*>
-                           v .: "PackageBaseID"        <*>
-                           v .: "PackageBase"          <*>
-                           v .: "Version"              <*>
-                           v .: "CategoryID"           <*>
-                           v .: "Description"          <*>
-                           v .: "License"              <*>
-                           v .: "URL"                  <*>
-                           v .: "NumVotes"             <*>
-                           v .: "OutOfDate"            <*>
-                           v .: "Maintainer"           <*>
-                           v .: "FirstSubmitted"       <*>
-                           v .: "LastModified"         <*>
-                           v .: "URLPath"              <*>
-                           (f <$> v .:? "Depends")     <*>
-                           (f <$> v .:? "MakeDepends") <*>
-                           (f <$> v .:? "OptDepends")  <*>
-                           (f <$> v .:? "Conflicts")   <*>
-                           (f <$> v .:? "Provides")
-                               where f = L.concat . maybeToList
+    parseJSON (Object v) = AurInfo                    <$>
+                           v .:  "ID"                 <*>
+                           v .:  "Name"               <*>
+                           v .:  "PackageBaseID"      <*>
+                           v .:  "PackageBase"        <*>
+                           v .:  "Version"            <*>
+                           v .:  "CategoryID"         <*>
+                           v .:  "Description"        <*>
+                           v .:  "License"            <*>
+                           v .:  "URL"                <*>
+                           v .:  "NumVotes"           <*>
+                           v .:  "OutOfDate"          <*>
+                           v .:  "Maintainer"         <*>
+                           v .:  "FirstSubmitted"     <*>
+                           v .:  "LastModified"       <*>
+                           v .:  "URLPath"            <*>
+                           v .:? "Depends"     .!= [] <*>
+                           v .:? "MakeDepends" .!= [] <*>
+                           v .:? "OptDepends"  .!= [] <*>
+                           v .:? "Conflicts"   .!= [] <*>
+                           v .:? "Provides"    .!= []
     parseJSON _          = mzero
