8d6cc1559ff507bf17108b969cc16d189e3275ee
Colin Woodbury <colingw@gmail.com>
2014-07-12 19:59:48 -0700

    Added top-level module
    - Exports all others

---
+++ Linux/Arch/Aur.hs
@@ -0,0 +1,8 @@
+module Linux.Arch.Aur
+    ( module Linux.Arch.Aur.Pkgbuild
+    , module Linux.Arch.Aur.Rpc
+    , module Linux.Arch.Aur.Types ) where
+
+import Linux.Arch.Aur.Pkgbuild
+import Linux.Arch.Aur.Rpc
+import Linux.Arch.Aur.Types
